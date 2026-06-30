1ab2c1a93236a4704b226692e3eb7044ed1d1198
Imran <imranswaleh10@gmail.com>
2026-06-30 09:13:46 +0300

    switch to postgres for production

--- clothing_store/settings.py
+++ clothing_store/settings.py
@@ -52,11 +52,13 @@ TEMPLATES = [{
 
 WSGI_APPLICATION = 'clothing_store.wsgi.application'
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 LANGUAGE_CODE = 'en-us'
